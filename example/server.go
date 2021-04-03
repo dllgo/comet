c19fa6eb857117453e4ad62fc1a7335d9e69390a
lcb <liuchengbin@laoyuegou.com>
2021-04-03 21:16:25 +0800

    up

--- example/server.go
+++ example/server.go
@@ -3,5 +3,12 @@ package main
 import "github.com/dllgo/comet"
 
 func main() {
-	comet.Serve(6000)
+	srv:=comet.NewComet()
+	srv.SetMsgHandler(React)
+	srv.Serve(6000)
+}
+func React(context.Context,frame []byte) (out []byte,err error) {
+	// Use ants pool to unblock the event-loop.
+	log.Println("[React] frame:", frame)
+ 
 }
